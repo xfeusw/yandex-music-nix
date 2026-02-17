d07bec2ac1bb2fb69edfeb74ebf636c2a9b0ac72
xfeusw <khamrakulovkamron@gmail.com>
2026-02-17 12:24:43 +0500

    fix: version changes

--- nix/package.nix
+++ nix/package.nix
@@ -58,11 +58,11 @@
 in
   stdenv.mkDerivation {
     pname = "yandex-music-bin";
-    version = "5.85.0";
+    version = "5.86.0";
 
     src = fetchurl {
       url = "https://desktop.app.music.yandex.net/stable/Yandex_Music_amd64_5.86.0.deb";
-      hash = "sha256-1astgZCU9H6GhVCSMMYbv0easZQLtEdoCDxQSimxjB0=";
+      hash = "sha256-cpinXpcRu/WnWwH/kq2wy7arifzzjXo/qM342hHGDF4=";
     };
 
     nativeBuildInputs = [dpkg makeWrapper pkgs.lswt pkgs.xdotool];
