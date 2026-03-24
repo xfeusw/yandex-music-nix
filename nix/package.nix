100878c1bc32123739898e8e11bc4090ece057e4
xfeusw <khamrakulovkamron@gmail.com>
2026-03-24 08:47:45 +0500

    refactor: version is updated

--- nix/package.nix
+++ nix/package.nix
@@ -36,7 +36,8 @@
   libxcb-keysyms,
   libxcb-render-util,
   pkgs,
-}: let
+}:
+let
   runtimeLibs = [
     glib
     gtk3
@@ -71,55 +72,60 @@
     libxcb-render-util
   ];
 in
-  stdenv.mkDerivation {
-    pname = "yandex-music-bin";
-    version = "5.91.1";
+stdenv.mkDerivation {
+  pname = "yandex-music-bin";
+  version = "5.92.1";
 
-    src = fetchurl {
-      url = "https://desktop.app.music.yandex.net/stable/Yandex_Music_amd64_5.91.1.deb";
-      hash = "sha256-Bl9Q2GVRJncDnCJqbOd6sLEKNVJ61WrJiJE9wmbkOvQ=";
-    };
+  src = fetchurl {
+    url = "https://desktop.app.music.yandex.net/stable/Yandex_Music_amd64_5.92.1.deb";
+    hash = "sha256-kOsGTNjRv64Li0kFCK70DiV2e7MlKJIrq9cUOFfwpGU=";
+  };
 
-    nativeBuildInputs = [dpkg makeWrapper pkgs.lswt pkgs.xdotool];
+  nativeBuildInputs = [
+    dpkg
+    makeWrapper
+    pkgs.lswt
+    pkgs.xdotool
+  ];
 
-    dontConfigure = true;
-    dontBuild = true;
+  dontConfigure = true;
+  dontBuild = true;
 
-    unpackPhase = "true";
+  unpackPhase = "true";
 
-    installPhase = ''
-      runHook preInstall
-      mkdir -p $out
-      dpkg-deb -x "$src" "$out"
-      if [ -d "$out/usr/share" ]; then
-        mkdir -p "$out/share"
-        cp -rT "$out/usr/share" "$out/share"
-        rm -rf "$out/usr/share"
-      fi
-      mkdir -p $out/bin
-      makeWrapper "$out/opt/Яндекс Музыка/yandexmusic" "$out/bin/yandexmusic" \
-        --prefix LD_LIBRARY_PATH : "${lib.makeLibraryPath runtimeLibs}" \
-        --set-default ELECTRON_IS_DEV 0
+  installPhase = ''
+    runHook preInstall
+    mkdir -p $out
+    dpkg-deb -x "$src" "$out"
+    if [ -d "$out/usr/share" ]; then
+      mkdir -p "$out/share"
+      cp -rT "$out/usr/share" "$out/share"
+      rm -rf "$out/usr/share"
+    fi
+    mkdir -p $out/bin
+    makeWrapper "$out/opt/Яндекс Музыка/yandexmusic" "$out/bin/yandexmusic" \
+      --prefix LD_LIBRARY_PATH : "${lib.makeLibraryPath runtimeLibs}" \
+      --set-default ELECTRON_IS_DEV 0
 
-      mkdir -p "$out/share/applications"
-      cat > "$out/share/applications/yandexmusic.desktop" << EOF
-      [Desktop Entry]
-      Name=Яндекс Музыка
-      Exec=$out/bin/yandexmusic %U
-      Terminal=false
-      Type=Application
-      Icon=yandexmusic
-      StartupWMClass=Яндекс Музыка
-      Comment=Personal recommendations, mixes for any occasion and the latest musical releases
-      MimeType=x-scheme-handler/yandexmusic;
-      Categories=Audio;
-      StartupNotify=true
-      EOF
+    mkdir -p "$out/share/applications"
+    cat > "$out/share/applications/yandexmusic.desktop" << EOF
+    [Desktop Entry]
+    Name=Яндекс Музыка
+    Exec=$out/bin/yandexmusic %U
+    Terminal=false
+    Type=Application
+    Icon=yandexmusic
+    StartupWMClass=Яндекс Музыка
+    Comment=Personal recommendations, mixes for any occasion and the latest musical releases
+    MimeType=x-scheme-handler/yandexmusic;
+    Categories=Audio;
+    StartupNotify=true
+    EOF
 
-      runHook postInstall
-    '';
+    runHook postInstall
+  '';
 
-    meta = {
-      platforms = ["x86_64-linux"];
-    };
-  }
+  meta = {
+    platforms = [ "x86_64-linux" ];
+  };
+}
